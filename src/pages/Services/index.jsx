f30cd7b2250b982a0664e8b7edea2e0a268b0178
henriquetty <gamercaipira@gmail.com>
2020-09-05 12:58:07 -0300

    removing unused imports

--- src/pages/Services/index.jsx
+++ src/pages/Services/index.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 
 import Navbar from "../../components/Navbar";
-import Hero from "../../components/Hero";
-import Cards from "../../components/Cards";
 
 function Services() {
   return (
